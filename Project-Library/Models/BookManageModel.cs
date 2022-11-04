7b0d089b392824b18039c49c1db732cabf83927c
REVISION-PC\Seth <sethetherald@duck.com>
2022-11-04 20:04:11 +0700

    Rename function from Manage Title to Manage Book.
    Load data into DataGridView.
    
    Add Filter and Refresh Function.
    
    Add new buttons.

---
+++ Project-Library/Models/BookManageModel.cs
@@ -0,0 +1,14 @@
+﻿namespace Project_Library.Models
+{
+    internal class BookManageModel
+    {
+        public int BookId { get; set; }
+        public int TitleId { get; set; }
+        public string? Title { get; set; }
+        public string? Author { get; set; }
+        public string? Publisher { get; set; }
+        public int InStock { get; set; }
+        public int NumberOfPages { get; set; }
+        public string? Condition { get; set; }
+    }
+}
