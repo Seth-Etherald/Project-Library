066a7144a49c80bfd1923ef7def8a20693e52b53
REVISION-PC\Seth <sethetherald@duck.com>
2022-11-07 17:56:53 +0700

    Modify model to match with altered database table.

--- Project-Library/Models/LendBookDetail.cs
+++ Project-Library/Models/LendBookDetail.cs
@@ -15,7 +15,7 @@ namespace Project_Library.Models
         public int LibrarianId { get; set; }
         public DateTime LendDate { get; set; }
         public DateTime ExpectedReturnDate { get; set; }
-        public string ReturnCondition { get; set; } = null!;
+        public int ReturnCondition { get; set; }
 
         public virtual Reader CardNumberNavigation { get; set; } = null!;
         public virtual Librarian Librarian { get; set; } = null!;
